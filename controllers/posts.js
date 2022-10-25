bef55e8738df74357b0a1ed9802851b601fbc090
rajea-bilal <rajea.bilal@gmail.com>
2022-10-25 05:54:27 +0100

    fixed the extra comma in the likePost method

--- controllers/posts.js
+++ controllers/posts.js
@@ -57,11 +57,7 @@ module.exports = {
   likePost: async (req, res) => {
     try {
       await Post.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-          $inc: { likes: 1 },
-        }
-      );
+        { _id: req.params.id },{ $inc: { likes: 1 }});
       console.log("Likes +1");
       res.redirect(`/post/${req.params.id}`);
     } catch (err) {
